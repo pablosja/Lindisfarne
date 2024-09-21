ed50947e8d757348e8f48fdb5f5532668f404894
Pablo-SanJose-Acedo <psanjosea@gmail.com>
2024-09-21 19:50:59 +0200

    first commit

---
+++ src/main/java/pablosja/lindisfarne/LindisfarneApplication.java
@@ -0,0 +1,13 @@
+package pablosja.lindisfarne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindisfarneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindisfarneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pablosja/lindisfarne/LindisfarneApplicationTests.java
@@ -0,0 +1,13 @@
+package pablosja.lindisfarne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindisfarneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
